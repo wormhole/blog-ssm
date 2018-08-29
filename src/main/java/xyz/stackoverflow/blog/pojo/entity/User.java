fed94c2ba85f108e082878ceb0ccaaec90a4fab4
wormhole1996 <363408268@qq.com>
2018-08-29 23:37:04 +0800

    添加User实体类

---
+++ src/main/java/xyz/stackoverflow/blog/pojo/entity/User.java
@@ -0,0 +1,61 @@
+package xyz.stackoverflow.blog.pojo.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String salt;
+    private String headurl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHeadurl() {
+        return headurl;
+    }
+
+    public void setHeadurl(String headurl) {
+        this.headurl = headurl;
+    }
+}
