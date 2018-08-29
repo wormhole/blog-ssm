df3bf999bb1d68fb644093483896f203eb72629b
wormhole1996 <363408268@qq.com>
2018-08-29 23:46:13 +0800

    添加自定义JdbcRealm

---
+++ src/main/java/xyz/stackoverflow/blog/web/shiro/realm/JdbcRealm.java
@@ -0,0 +1,32 @@
+package xyz.stackoverflow.blog.web.shiro.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.SimpleByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import xyz.stackoverflow.blog.pojo.entity.User;
+import xyz.stackoverflow.blog.service.UserService;
+
+public class JdbcRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();
+        User user = userService.getUserByEmail(username);
+        SimpleAuthenticationInfo sa = new SimpleAuthenticationInfo(user.getEmail(),user.getPassword(),new SimpleByteSource(user.getSalt()),getName());
+        return sa;
+    }
+}
