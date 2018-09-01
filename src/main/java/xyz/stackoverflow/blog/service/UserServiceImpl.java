f4fec3a1d8762cc667f3198943b67b6841b481e1
wormhole1996 <363408268@qq.com>
2018-09-02 00:14:44 +0800

    修改UserServiceImpl类 增加插入用户前置规则

--- src/main/java/xyz/stackoverflow/blog/service/UserServiceImpl.java
+++ src/main/java/xyz/stackoverflow/blog/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.stackoverflow.blog.dao.UserDao;
 import xyz.stackoverflow.blog.pojo.entity.User;
+import xyz.stackoverflow.blog.util.IdGenerator;
+import xyz.stackoverflow.blog.util.PasswordUtil;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -13,31 +15,35 @@ public class UserServiceImpl implements UserService{
     private UserDao dao;
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public User getUserByEmail(String email) {
         return dao.getUserByEmail(email);
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public int addUser(User user) {
+        user.setHeadurl("/static/custom/image/head.png");
+        user.setId(IdGenerator.getId());
+        user.setSalt(PasswordUtil.getSalt());
+        user.setPassword(PasswordUtil.encryptPassword(user.getSalt(),user.getPassword()));
         return dao.addUser(user);
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public int updateHeadurl(User user) {
         return dao.updateHeadurl(user);
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public int updatePassword(User user) {
         return dao.updatePassword(user);
     }
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public int udpateNickname(User user) {
         return dao.updateNickname(user);
     }
