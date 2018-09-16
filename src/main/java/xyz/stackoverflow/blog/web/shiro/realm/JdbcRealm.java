2fa791dad7ec220a8e3cbbe72a9d00dd7e8c0296
wormhole1996 <363408268@qq.com>
2018-09-17 01:27:18 +0800

    jdbcRealm添加权限获取

--- src/main/java/xyz/stackoverflow/blog/web/shiro/realm/JdbcRealm.java
+++ src/main/java/xyz/stackoverflow/blog/web/shiro/realm/JdbcRealm.java
@@ -5,28 +5,48 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import xyz.stackoverflow.blog.pojo.entity.User;
+import xyz.stackoverflow.blog.service.PermissionService;
+import xyz.stackoverflow.blog.service.RoleService;
 import xyz.stackoverflow.blog.service.UserService;
 import xyz.stackoverflow.blog.web.shiro.util.SimpleByteSource;
+
+import java.util.Set;
 
 public class JdbcRealm extends AuthorizingRealm {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RoleService roleService;
+    @Autowired
+    private PermissionService permissionService;
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        String email = (String) principalCollection.getPrimaryPrincipal();
+        User user = userService.getUserByEmail(email);
+        SimpleAuthorizationInfo sa = new SimpleAuthorizationInfo();
+        Set<String> roleSet = roleService.getRoleByUserId(user.getId());
+        if (roleSet != null) {
+            Set<String> permissionSet = permissionService.getAllPermission(roleSet.toArray(new String[0]));
+            sa.setRoles(roleSet);
+            if (permissionSet != null) {
+                sa.setStringPermissions(permissionSet);
+            }
+        }
+        return sa;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         String email = (String) authenticationToken.getPrincipal();
         User user = userService.getUserByEmail(email);
-        SimpleAuthenticationInfo sa = new SimpleAuthenticationInfo(user.getEmail(),user.getPassword(),new SimpleByteSource(user.getSalt()),getName());
+        SimpleAuthenticationInfo sa = new SimpleAuthenticationInfo(user.getEmail(), user.getPassword(), new SimpleByteSource(user.getSalt()), getName());
         return sa;
     }
 }
--- src/main/webapp/WEB-INF/view/admin/index.ftl
+++ src/main/webapp/WEB-INF/view/admin/index.ftl
@@ -41,7 +41,6 @@
                         <dd><a href="javascript:;" data-url="" data-title="所有文章" data-id="">所有文章</a></dd>
                         <dd><a href="javascript:;" data-url="/admin/article/write" data-title="写文章" data-id="write">写文章</a></dd>
                         <dd><a href="javascript:;" data-url="" data-title="分类" data-id="">分类</a></dd>
-                        <dd><a href="javascript:;" data-url="" data-title="标签" data-id="">标签</a></dd>
                     </dl>
                 </li>
                 <li class="layui-nav-item">
