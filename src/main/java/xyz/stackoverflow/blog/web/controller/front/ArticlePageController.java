a79ad91b949ee639ba0f9d1f39fca78307d5f720
wormhole1996 <363408268@qq.com>
2018-10-21 19:38:32 +0800

    前端页面跳转控制器添加文档注释

--- src/main/java/xyz/stackoverflow/blog/web/controller/front/ArticlePageController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/front/ArticlePageController.java
@@ -6,10 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 前端页面跳转控制器
+ *
+ * @author 凉衫薄
+ */
 @Controller
 @RequestMapping(value = "/article", method = RequestMethod.GET)
 public class ArticlePageController {
 
+    /**
+     * 跳转到文章单页 /article/{year}/{month}/{day}/{articleCode}
+     * 方法 GET
+     *
+     * @param year
+     * @param month
+     * @param day
+     * @param articleCode
+     * @return 返回ModelAndView
+     */
     @RequestMapping("/{year}/{month}/{day}/{articleCode}")
     public ModelAndView article(@PathVariable("year") String year, @PathVariable("month") String month, @PathVariable("day") String day, @PathVariable("articleCode") String articleCode) {
         ModelAndView mv = new ModelAndView();
--- src/main/java/xyz/stackoverflow/blog/web/controller/front/CategoryPageController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/front/CategoryPageController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 前端页面跳转控制器
+ *
+ * @author 凉衫薄
+ */
 @Controller
 public class CategoryPageController {
 
-    @RequestMapping(value="/category/{categoryCode}",method=RequestMethod.GET)
-    public ModelAndView category(){
+    /**
+     * 跳转到某一分类所有文章显示页面 /category/{categoryCode}
+     * 方法 GET
+     *
+     * @return 返回ModelAndView
+     */
+    @RequestMapping(value = "/category/{categoryCode}", method = RequestMethod.GET)
+    public ModelAndView category() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/category");
         return mv;
