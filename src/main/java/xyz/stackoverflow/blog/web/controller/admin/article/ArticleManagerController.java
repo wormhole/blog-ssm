b2e1a8d1a7ba6f515807e668c68559c10adeffe6
wormhole1996 <363408268@qq.com>
2018-10-21 18:51:07 +0800

    文章管理控制器添加文档注释

--- src/main/java/xyz/stackoverflow/blog/web/controller/admin/article/ArticleManagerController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/admin/article/ArticleManagerController.java
@@ -9,7 +9,7 @@ import xyz.stackoverflow.blog.service.ArticleService;
 import xyz.stackoverflow.blog.service.CategoryService;
 import xyz.stackoverflow.blog.service.UserService;
 import xyz.stackoverflow.blog.util.PageParameter;
-import xyz.stackoverflow.blog.util.ResponseJson;
+import xyz.stackoverflow.blog.pojo.vo.ResponseVO;
 import xyz.stackoverflow.blog.validator.ArticleValidator;
 
 import java.text.SimpleDateFormat;
@@ -18,6 +18,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 后台管理系统文章管理控制器
+ *
+ * @author 凉衫薄
+ */
 @Controller
 @RequestMapping("/admin/article")
 public class ArticleManagerController {
@@ -34,23 +39,28 @@ public class ArticleManagerController {
     @Autowired
     private ArticleValidator articleValidator;
 
+    /**
+     * 获取文章 /admin/article/list
+     * 方法 GET
+     *
+     * @param page 分页查询页码,允许为空,为空时查询所有文章
+     * @param limit 每页的条目
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseJson list(@RequestParam(value = "page", required = false) String page, @RequestParam(value = "limit", required = false) String limit) {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO list(@RequestParam(value = "page", required = false) String page, @RequestParam(value = "limit", required = false) String limit) {
+        ResponseVO response = new ResponseVO();
         List<Article> list = null;
         if (page != null && limit != null) {
-            PageParameter pageParameter = new PageParameter();
-            pageParameter.setPageNo(Integer.valueOf(page));
-            pageParameter.setLimit(Integer.valueOf(limit));
-            pageParameter.setStart((pageParameter.getPageNo() - 1) * pageParameter.getLimit());
+            PageParameter pageParameter = new PageParameter(Integer.valueOf(page),Integer.valueOf(limit),null);
             list = articleService.getLimitArticle(pageParameter);
-
         } else {
             list = articleService.getAllArticle();
         }
+
         int count = articleService.getArticleCount();
-        List<ArticleVO> voList = new ArrayList<>();
+        List voList = new ArrayList<ArticleVO>();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         for (Article article : list) {
             ArticleVO vo = new ArticleVO();
@@ -58,7 +68,7 @@ public class ArticleManagerController {
             vo.setTitle(article.getTitle());
             vo.setNickname(userService.getUserById(article.getUserId()).getNickname());
             vo.setCategoryName(categoryService.getCategoryById(article.getCategoryId()).getCategoryName());
-            vo.setDate(sdf.format(article.getDate()));
+            vo.setDateString(sdf.format(article.getDate()));
             vo.setUrl(article.getUrl());
             voList.add(vo);
         }
@@ -72,10 +82,17 @@ public class ArticleManagerController {
         return response;
     }
 
+    /**
+     * 文章删除 /admin/article/delete
+     * 方法 POST
+     *
+     * @param articleVO 文章VO数组
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseJson delete(@RequestBody ArticleVO[] articleVO) {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO delete(@RequestBody ArticleVO[] articleVO) {
+        ResponseVO response = new ResponseVO();
         for (ArticleVO vo : articleVO)
             articleService.deleteArticleById(vo.getId());
         response.setStatus(SUCCESS);
@@ -83,10 +100,17 @@ public class ArticleManagerController {
         return response;
     }
 
+    /**
+     * 获取单篇文章 /admin/article/get
+     * 方法 POST
+     *
+     * @param articleVO 文章VO
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/get", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseJson get(@RequestBody ArticleVO articleVO) {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO get(@RequestBody ArticleVO articleVO) {
+        ResponseVO response = new ResponseVO();
         Article article = articleService.getArticleById(articleVO.getId());
         ArticleVO vo = null;
         if (article != null) {
@@ -106,10 +130,17 @@ public class ArticleManagerController {
         return response;
     }
 
+    /**
+     * 更新文章 /admin/article/update
+     * 方法 POST
+     *
+     * @param articleVO 文章VO
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseJson update(@RequestBody ArticleVO articleVO) {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO update(@RequestBody ArticleVO articleVO) {
+        ResponseVO response = new ResponseVO();
         Article article = articleService.getArticleById(articleVO.getId());
 
         Map map = articleValidator.validate(articleVO);
@@ -121,9 +152,8 @@ public class ArticleManagerController {
             if (!article.getUrl().equals(articleVO.getUrl()) && articleService.isExistUrl(articleVO.getUrl())) {
                 response.setStatus(FAILURE);
                 response.setMessage("URL重复");
-                Map map1 = new HashMap<String, String>();
-                map1.put("url", "URL重复");
-                response.setData(map1);
+                map.put("url", "URL重复");
+                response.setData(map);
             }else {
                 Article updateArticle = articleVO.toArticle();
                 articleService.updateArticle(updateArticle);
