121340bdfb129f47bc5413f8c20cfb3e776fde95
wormhole1996 <363408268@qq.com>
2018-10-21 15:23:34 +0800

    异常类添加类注释

--- src/main/java/xyz/stackoverflow/blog/exception/IncorrectVCodeException.java
+++ src/main/java/xyz/stackoverflow/blog/exception/IncorrectVCodeException.java
@@ -1,5 +1,10 @@
 package xyz.stackoverflow.blog.exception;
 
+/**
+ * @Author: 凉衫薄
+ * @Date: 2018-10-21
+ * @Description: 验证码错误异常类
+ */
 public class IncorrectVCodeException extends Exception {
     public IncorrectVCodeException(String message) {
         super(message);
