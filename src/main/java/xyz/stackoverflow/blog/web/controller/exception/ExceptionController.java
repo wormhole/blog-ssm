2e5a92d15d760b5b79767d1e4d15c319618ff5a3
wormhole1996 <363408268@qq.com>
2018-10-21 19:41:01 +0800

    全局异常处理控制器添加文档注释

--- src/main/java/xyz/stackoverflow/blog/web/controller/exception/ExceptionController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/exception/ExceptionController.java
@@ -4,20 +4,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import xyz.stackoverflow.blog.util.ResponseJson;
+import xyz.stackoverflow.blog.pojo.vo.ResponseVO;
 
+/**
+ * 全局异常处理控制器
+ *
+ * @author 凉衫薄
+ */
 public class ExceptionController {
 
     private final Integer exceptionStatus = -1;
 
+    /**
+     * 所有异常统一处理
+     *
+     * @param e 异常对象
+     * @return 返回ResponseVO
+     */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public ResponseJson handleException(Exception e){
-        ResponseJson responseJson = new ResponseJson();
-        responseJson.setStatus(exceptionStatus);
-        responseJson.setMessage(e.getClass().getSimpleName());
-        responseJson.setData(e);
-        return responseJson;
+    public ResponseVO handleException(Exception e){
+        ResponseVO response = new ResponseVO();
+        response.setStatus(exceptionStatus);
+        response.setMessage(e.getClass().getSimpleName());
+        response.setData(e);
+        return response;
     }
 }
