125f0ba641f87813282c608d6288d7de2a655482
wormhole1996 <363408268@qq.com>
2018-10-21 19:11:14 +0800

    配置获取控制器添加文档注释

--- src/main/java/xyz/stackoverflow/blog/web/controller/api/ConfigController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/api/ConfigController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xyz.stackoverflow.blog.pojo.entity.User;
-import xyz.stackoverflow.blog.pojo.vo.PersonalInfoVO;
+import xyz.stackoverflow.blog.pojo.vo.UserVO;
 import xyz.stackoverflow.blog.service.UserService;
-import xyz.stackoverflow.blog.util.ResponseJson;
+import xyz.stackoverflow.blog.pojo.vo.ResponseVO;
 
+/**
+ * 配置获取控制器
+ *
+ * @author 凉衫薄
+ */
 @Controller
 @RequestMapping("/api")
 public class ConfigController {
@@ -20,20 +25,26 @@ public class ConfigController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 获取侧边栏信息 /api/sideinfo
+     * 方法 GET
+     *
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/sideinfo", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseJson user() {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO user() {
+        ResponseVO response = new ResponseVO();
         User admin = userService.getAdmin();
         if (admin != null) {
-            PersonalInfoVO personalInfoVO = new PersonalInfoVO();
-            personalInfoVO.setEmail(admin.getEmail());
-            personalInfoVO.setNickname(admin.getNickname());
-            personalInfoVO.setSignature(admin.getSignature());
-            personalInfoVO.setHeadUrl(admin.getHeadUrl());
+            UserVO userVO = new UserVO();
+            userVO.setEmail(admin.getEmail());
+            userVO.setNickname(admin.getNickname());
+            userVO.setSignature(admin.getSignature());
+            userVO.setHeadUrl(admin.getHeadUrl());
             response.setStatus(SUCCESS);
             response.setMessage("获取成功");
-            response.setData(personalInfoVO);
+            response.setData(userVO);
         }else{
             response.setStatus(FAILURE);
             response.setMessage("获取失败");
