2ba0edcc94a42250f60a5a35978a04b52991150e
wormhole1996 <363408268@qq.com>
2018-10-21 11:26:59 +0800

    拦截器做初始化设置

---
+++ src/main/java/xyz/stackoverflow/blog/web/interceptor/InitInterceptor.java
@@ -0,0 +1,34 @@
+package xyz.stackoverflow.blog.web.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import xyz.stackoverflow.blog.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InitInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (userService.getUserCount() == 0) {
+            response.sendRedirect("/admin/register");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
