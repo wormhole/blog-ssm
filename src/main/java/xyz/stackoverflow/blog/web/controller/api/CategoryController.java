e6e256689aab0376bf2b77b501328790b1269551
wormhole1996 <363408268@qq.com>
2018-10-21 19:34:59 +0800

    前端接口分类控制器添加文档注释

--- src/main/java/xyz/stackoverflow/blog/web/controller/api/CategoryController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/api/CategoryController.java
@@ -11,7 +11,7 @@ import xyz.stackoverflow.blog.service.ArticleService;
 import xyz.stackoverflow.blog.service.CategoryService;
 import xyz.stackoverflow.blog.service.UserService;
 import xyz.stackoverflow.blog.util.PageParameter;
-import xyz.stackoverflow.blog.util.ResponseJson;
+import xyz.stackoverflow.blog.pojo.vo.ResponseVO;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -19,6 +19,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 前端接口分类控制器
+ *
+ * @author 凉衫薄
+ */
 @Controller
 @RequestMapping("/api")
 public class CategoryController {
@@ -33,17 +38,23 @@ public class CategoryController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 分类获取 /api/category
+     * 方法 GET
+     *
+     * @return 返回ResponseVO
+     */
     @RequestMapping(value = "/category", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseJson getCategory() {
-        ResponseJson response = new ResponseJson();
+    public ResponseVO getCategory() {
+        ResponseVO response = new ResponseVO();
         List<Category> categoryList = categoryService.getAllCategory();
-        List<CategoryVO> categoryVOList = new ArrayList();
+        List<CategoryVO> categoryVOList = new ArrayList<>();
         for (Category category : categoryList) {
             CategoryVO categoryVO = new CategoryVO();
             categoryVO.setCategoryCode(category.getCategoryCode());
             categoryVO.setCategoryName(category.getCategoryName());
-            categoryVO.setArticleNum(articleService.getArticleByCategoryId(category.getId()).size());
+            categoryVO.setArticleCount(articleService.getArticleByCategoryId(category.getId()).size());
             categoryVOList.add(categoryVO);
         }
         response.setStatus(SUCCESS);
@@ -52,21 +63,25 @@ public class CategoryController {
         return response;
     }
 
-    @RequestMapping(value="/category/{categoryCode}",method=RequestMethod.GET)
+    /**
+     * 某一分类文章获取 /api/category/{categoryCode}
+     * 方法 GET
+     *
+     * @param categoryCode 分类编码
+     * @param page         分页查询参数,允许为空,为空时返回所有该分类文章
+     * @return 返回ResponseVO
+     */
+    @RequestMapping(value = "/category/{categoryCode}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseJson getArticleByCategoryCode(@PathVariable("categoryCode") String categoryCode,@RequestParam(value = "page", required = false) Integer page){
-        ResponseJson response = new ResponseJson();
-        Map map = new HashMap<String, Object>();
+    public ResponseVO getArticleByCategoryCode(@PathVariable("categoryCode") String categoryCode, @RequestParam(value = "page", required = false) Integer page) {
+        ResponseVO response = new ResponseVO();
+        Map<String,Object> map = new HashMap<>();
         List<Article> list = null;
-        List<ArticleVO> voList = new ArrayList();
+        List<ArticleVO> voList = new ArrayList<>();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Category category = categoryService.getCategoryByCode(categoryCode);
         if (page != null) {
-            PageParameter parameter = new PageParameter();
-            parameter.setPageNo(page);
-            parameter.setLimit(5);
-            parameter.setStart((parameter.getPageNo() - 1) * parameter.getLimit());
-            parameter.setWhere(category.getId());
+            PageParameter parameter = new PageParameter(page, 5, category.getId());
             list = articleService.getLimitArticleByCategoryId(parameter);
             map.put("page", page);
         } else {
@@ -78,7 +93,7 @@ public class CategoryController {
             vo.setArticleHtml(article.getArticleHtml());
             vo.setNickname(userService.getUserById(article.getUserId()).getNickname());
             vo.setCategoryName(categoryService.getCategoryById(article.getCategoryId()).getCategoryName());
-            vo.setDate(sdf.format(article.getDate()));
+            vo.setDateString(sdf.format(article.getDate()));
             vo.setUrl(article.getUrl());
             voList.add(vo);
         }
