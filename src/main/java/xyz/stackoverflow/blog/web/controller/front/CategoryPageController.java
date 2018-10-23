93371403819ddbbca72e6adbeca89d4d9e7550e4
wormhole1996 <363408268@qq.com>
2018-10-24 00:36:10 +0800

    重写分类页面

--- src/main/java/xyz/stackoverflow/blog/web/controller/front/CategoryPageController.java
+++ src/main/java/xyz/stackoverflow/blog/web/controller/front/CategoryPageController.java
@@ -1,16 +1,32 @@
 package xyz.stackoverflow.blog.web.controller.front;
 
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.HtmlUtils;
+import xyz.stackoverflow.blog.pojo.entity.Article;
+import xyz.stackoverflow.blog.pojo.entity.Category;
+import xyz.stackoverflow.blog.pojo.entity.User;
+import xyz.stackoverflow.blog.pojo.vo.ArticleVO;
+import xyz.stackoverflow.blog.pojo.vo.CategoryVO;
+import xyz.stackoverflow.blog.pojo.vo.UserVO;
+import xyz.stackoverflow.blog.service.ArticleService;
 import xyz.stackoverflow.blog.service.CategoryService;
+import xyz.stackoverflow.blog.service.UserService;
+import xyz.stackoverflow.blog.util.PageParameter;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 前端页面跳转控制器
+ * 前端分类页面跳转控制器
  *
  * @author 凉衫薄
  */
@@ -19,22 +35,92 @@ public class CategoryPageController {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ArticleService articleService;
 
     /**
      * 跳转到某一分类所有文章显示页面 /category/{categoryCode}
      * 方法 GET
      *
-     * @return 返回ModelAndView,查找成功时返回分类页面,否则返回404页面
+     * @return 返回ModelAndView, 查找成功时返回分类页面, 否则返回404页面
      */
     @RequestMapping(value = "/category/{categoryCode}", method = RequestMethod.GET)
-    public ModelAndView category(@PathVariable("categoryCode") String categoryCode) {
+    public ModelAndView categoryArticle(@PathVariable("categoryCode") String categoryCode, @RequestParam(value = "page", required = false, defaultValue = "1") String page) {
         ModelAndView mv = new ModelAndView();
-        if(categoryService.isExistCode(categoryCode)) {
-            mv.setViewName("/category");
-        }else{
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        User admin = userService.getAdmin();
+        UserVO userVO = new UserVO();
+        userVO.setNickname(HtmlUtils.htmlEscape(admin.getNickname()));
+        userVO.setSignature(HtmlUtils.htmlEscape(admin.getSignature()));
+        userVO.setHeadUrl(admin.getHeadUrl());
+
+        Category category = categoryService.getCategoryByCode(categoryCode);
+        if (category != null) {
+            PageParameter parameter = new PageParameter(Integer.valueOf(page), 5, category.getId());
+            List<Article> articleList = articleService.getLimitArticleByCategoryId(parameter);
+            List<ArticleVO> articleVOList = new ArrayList<>();
+            for (Article article : articleList) {
+                ArticleVO vo = new ArticleVO();
+                vo.setTitle(HtmlUtils.htmlEscape(article.getTitle()));
+                vo.setNickname(HtmlUtils.htmlEscape(userService.getUserById(article.getUserId()).getNickname()));
+                vo.setCategoryName(categoryService.getCategoryById(article.getCategoryId()).getCategoryName());
+                vo.setHits(article.getHits());
+                vo.setPreview(Jsoup.parse(article.getArticleHtml()).text());
+                vo.setUrl(article.getUrl());
+                vo.setCreateDateString(sdf.format(article.getCreateDate()));
+                articleVOList.add(vo);
+            }
+
+            int count = articleService.getArticleCountByCategoryId(category.getId());
+            int pageCount = (count % 5 == 0) ? count / 5 : count / 5 + 1;
+            int start = (Integer.valueOf(page) - 2 < 1) ? 1 : Integer.valueOf(page) - 2;
+            int end = (start + 4 > pageCount) ? pageCount : start + 4;
+            if ((end - start) < 4) {
+                start = (end - 4 < 1) ? 1 : end - 4;
+            }
+
+            mv.addObject("user", userVO);
+            mv.addObject("articleList", articleVOList);
+            mv.addObject("start", start);
+            mv.addObject("end", end);
+            mv.addObject("page", Integer.valueOf(page));
+            mv.addObject("pageCount", pageCount);
+            mv.addObject("path", "/category/" + categoryCode);
+            mv.setViewName("/index");
+        } else {
             mv.setStatus(HttpStatus.NOT_FOUND);
+            mv.addObject("user", userVO);
             mv.setViewName("/error/404");
         }
+        return mv;
+    }
+
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
+    public ModelAndView category() {
+        ModelAndView mv = new ModelAndView();
+
+        User admin = userService.getAdmin();
+        UserVO userVO = new UserVO();
+        userVO.setNickname(HtmlUtils.htmlEscape(admin.getNickname()));
+        userVO.setSignature(HtmlUtils.htmlEscape(admin.getSignature()));
+        userVO.setHeadUrl(admin.getHeadUrl());
+
+        List<Category> categoryList = categoryService.getAllCategory();
+        List<CategoryVO> categoryVOList = new ArrayList<>();
+        for (Category category : categoryList) {
+            CategoryVO vo = new CategoryVO();
+            vo.setCategoryName(category.getCategoryName());
+            vo.setCategoryCode(category.getCategoryCode());
+            vo.setArticleCount(articleService.getArticleCountByCategoryId(category.getId()));
+            categoryVOList.add(vo);
+        }
+
+        mv.addObject("user", userVO);
+        mv.addObject("categoryList", categoryVOList);
+        mv.setViewName("/category");
         return mv;
     }
 }
---
+++ src/main/webapp/WEB-INF/view/category.ftl
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="/static/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/static/open-iconic/font/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/static/custom/css/category.css">
+    <script src="/static/jquery/jquery.min.js"></script>
+    <script src="/static/bootstrap/js/bootstrap.min.js"></script>
+    <title>溢栈</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="left">
+                <img src="${user.headUrl}" id="head" class="rounded-circle">
+                <div class="nickname" id="nickname">${user.nickname}</div>
+                <hr/>
+                <div class="signature" id="signature">${user.signature}</div>
+                <div class="menu">
+                    <a class="item btn" href="/">
+                        首页
+                    </a>
+                    <a class="item select btn" href="/category">
+                        分类
+                    </a>
+                    <a class="item btn" href="http://wpa.qq.com/msgrd?v=3&uin=363408268&site=qq&menu=yes">
+                        与我聊聊
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="middle">
+                <div class="header">
+                    文章分类
+                </div>
+                <div class="body">
+                    <#list categoryList as category>
+                        <div class="category">
+                            <a class="btn btn-secondary" href="/category/${category.categoryCode}">
+                                <span>${category.categoryName}</span>
+                                <span class="badge badge-light">${category.articleCount}</span>
+                            </a>
+                        </div>
+                    </#list>
+                </div>
+            </div>
+            <div class="buttom">
+            </div>
+            <div class="padding">
+            </div>
+        </div>
+    </div>
+</div>
+<footer>copyright &copy; 2018 by 溢栈</footer>
+</body>
+</html>
