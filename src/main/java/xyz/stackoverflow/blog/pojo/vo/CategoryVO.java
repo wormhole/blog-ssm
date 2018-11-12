6b82d95c46236eb121f7db5bc38741b099e2d2a6
wormhole1996 <363408268@qq.com>
2018-11-12 20:18:37 +0800

    修改分类验证规则

--- src/main/java/xyz/stackoverflow/blog/pojo/vo/CategoryVO.java
+++ src/main/java/xyz/stackoverflow/blog/pojo/vo/CategoryVO.java
@@ -1,7 +1,11 @@
 package xyz.stackoverflow.blog.pojo.vo;
 
+import org.hibernate.validator.constraints.Length;
 import xyz.stackoverflow.blog.pojo.entity.Category;
 import xyz.stackoverflow.blog.util.web.SuperVO;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * 分类VO
@@ -11,14 +15,23 @@ import xyz.stackoverflow.blog.util.web.SuperVO;
 public class CategoryVO implements SuperVO {
 
     private String id;
+
+    @NotNull(message = "分类名不能为空")
+    @Length(min = 1, max = 20, message = "长度只能在1到20之间")
+    @Pattern(regexp = "^[\\u4e00-\\u9fa50-9a-zA-Z_]+$", message = "分类名只能包含中文数字字母下划线")
     private String categoryName;
+
+    @NotNull(message = "编码不能为空")
+    @Length(min = 1, max = 20, message = "长度只能在1到20之间")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "编码只能为字母数字下划线")
     private String categoryCode;
+
     private Integer deleteAble;
 
     private Integer articleCount;
     private String deleteTag;
 
-    public CategoryVO(){
+    public CategoryVO() {
 
     }
 
@@ -84,7 +97,7 @@ public class CategoryVO implements SuperVO {
      *
      * @return 转换后的实体类
      */
-    public Category toCategory(){
+    public Category toCategory() {
         Category category = new Category();
         category.setId(id);
         category.setCategoryCode(categoryCode);
