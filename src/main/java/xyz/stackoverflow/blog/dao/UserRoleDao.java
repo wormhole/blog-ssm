f627ad6ccbb7121e12d4f71b9e6fdda9929f3464
wormhole1996 <363408268@qq.com>
2018-12-14 16:56:40 +0800

    重写UserRole表mapper

--- src/main/java/xyz/stackoverflow/blog/dao/UserRoleDao.java
+++ src/main/java/xyz/stackoverflow/blog/dao/UserRoleDao.java
@@ -2,8 +2,10 @@ package xyz.stackoverflow.blog.dao;
 
 import org.springframework.stereotype.Repository;
 import xyz.stackoverflow.blog.pojo.entity.UserRole;
+import xyz.stackoverflow.blog.util.db.PageParameter;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 用户-角色表DAO
@@ -12,6 +14,26 @@ import java.util.List;
  */
 @Repository
 public interface UserRoleDao {
+
+    List<UserRole> selectByPage(PageParameter pageParameter);
+
+    List<UserRole> selectByCondition(Map<String, String> searchMap);
+
+    UserRole selectById(String id);
+
+    int insert(UserRole userRole);
+
+    int batchInsert(List<UserRole> list);
+
+    int deleteById(String id);
+
+    int batchDeleteById(List<String> list);
+
+    int update(UserRole userRole);
+
+    int batchUpdate(List<UserRole> list);
+
+
     List<UserRole> getUserRoleByUserId(String userId);
 
     int insertUserRole(UserRole userRole);
