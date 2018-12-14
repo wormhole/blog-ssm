ac2bd0edfd4fc692b23f90f6aa351e05666ff17a
wormhole1996 <363408268@qq.com>
2018-12-14 15:13:42 +0800

    角色权限中间表mapper重写

--- src/main/java/xyz/stackoverflow/blog/dao/RolePermissionDao.java
+++ src/main/java/xyz/stackoverflow/blog/dao/RolePermissionDao.java
@@ -2,8 +2,10 @@ package xyz.stackoverflow.blog.dao;
 
 import org.springframework.stereotype.Repository;
 import xyz.stackoverflow.blog.pojo.entity.RolePermission;
+import xyz.stackoverflow.blog.util.db.PageParameter;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 角色-权限表DAO
@@ -12,6 +14,27 @@ import java.util.List;
  */
 @Repository
 public interface RolePermissionDao {
+
+    List<RolePermission> selectByPage(PageParameter pageParameter);
+
+    List<RolePermission> selectByCondition(Map<String,String> searchMap);
+
+    RolePermission selectById(String id);
+
+    int insert(RolePermission rolePermission);
+
+    int batchInsert(List<RolePermission> list);
+
+    int deleteById(String id);
+
+    int batchDeleteById(List<String> list);
+
+    int update(String id);
+
+    int batchUpdate(List<RolePermission> list);
+
+
     List<RolePermission> getRolePermissionByRoleId(String roleId);
+
     RolePermission insertRolePermission(RolePermission rolePermission);
 }
